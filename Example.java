2f38b2d340015966c5fb15a8402c985b2a7780f0
Menna Swilam <66182218+menna-swilam@users.noreply.github.com>
2023-03-29 21:40:12 +0200

    Update and rename Test.java to Example.java

---
+++ Example.java
@@ -0,0 +1,21 @@
+
+public class Example {
+
+
+
+        public static void main(String[] args) {
+            Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+            System.out.println("Enter number");
+
+            int userName = myObj.nextInt();
+            if (true) {
+
+            } else {
+
+            }
+            while(userName>0){
+                userName--;
+                continue;
+            }
+        }
+    }
